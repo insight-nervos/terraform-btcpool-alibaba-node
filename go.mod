25f07078639a3058aa9575db38844810b166c46b
Richard Mah <dick.mah@gmail.com>
2020-10-22 15:22:09 -0400

    update default test

---
+++ go.mod
@@ -0,0 +1,8 @@
+module test
+
+go 1.15
+
+require (
+	github.com/gruntwork-io/terratest v0.30.12
+	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897
+)
--- test/terraform_defaults_test.go
+++ test/terraform_defaults_test.go
@@ -1,12 +1,10 @@
 package test
 
 import (
-	"fmt"
-	"github.com/gruntwork-io/terratest/modules/aws"
-	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
-	"io/ioutil"
+	"log"
+	"os"
 	"path"
 	"testing"
 )
@@ -14,57 +12,34 @@ import (
 func TestTerraformDefaults(t *testing.T) {
 	t.Parallel()
 
-	exampleFolder := test_structure.CopyTerraformFolderToTemp(t, "../", "examples/defaults")
+	exampleDir := test_structure.CopyTerraformFolderToTemp(t, "../", "examples/defaults")
 
-	defer test_structure.RunTestStage(t, "teardown", func() {
-		terraformOptions := test_structure.LoadTerraformOptions(t, exampleFolder)
-		terraform.Destroy(t, terraformOptions)
-
-		keyPair := test_structure.LoadEc2KeyPair(t, exampleFolder)
-		aws.DeleteEC2KeyPair(t, keyPair)
-	})
-
-	test_structure.RunTestStage(t, "setup", func() {
-		terraformOptions, keyPair := configureTerraformOptions(t, exampleFolder)
-		test_structure.SaveTerraformOptions(t, exampleFolder, terraformOptions)
-		test_structure.SaveEc2KeyPair(t, exampleFolder, keyPair)
-
-		terraform.InitAndApply(t, terraformOptions)
-	})
-}
-
-
-func configureTerraformOptions(t *testing.T, exampleFolder string) (*terraform.Options, *aws.Ec2Keypair) {
-
-	uniqueID := random.UniqueId()
-	awsRegion := aws.GetRandomStableRegion(t, nil, nil)
-
-	keyPairName := fmt.Sprintf("terratest-ssh-example-%s", uniqueID)
-	keyPair := aws.CreateAndImportEC2KeyPair(t, awsRegion, keyPairName)
-
-	privateKeyPath := path.Join(exampleFolder, "id_rsa")
-	err := ioutil.WriteFile(privateKeyPath, []byte(keyPair.PrivateKey), 0600)
+	cwd, err := os.Getwd()
 	if err != nil {
-		panic(err)
+		log.Println(err)
 	}
 
-	publicKeyPath := path.Join(exampleFolder, "id_rsa.pub")
-	err = ioutil.WriteFile(publicKeyPath, []byte(keyPair.PublicKey), 0644)
-	if err != nil {
-		panic(err)
-	}
+	fixturesDir := path.Join(cwd, "fixtures")
+	privateKeyPath := path.Join(fixturesDir, "./keys/id_rsa_test")
+	publicKeyPath := path.Join(fixturesDir, "./keys/id_rsa_test.pub")
+	generateKeys(privateKeyPath, publicKeyPath)
 
 	terraformOptions := &terraform.Options{
-		TerraformDir: exampleFolder,
+		// The path to where our Terraform code is located
+		TerraformDir: exampleDir,
 
 		// Variables to pass to our Terraform code using -var options
 		Vars: map[string]interface{}{
-			"aws_region":    awsRegion,
-			//"public_key_path":    publicKeyPath,
-			//"private_key_path": privateKeyPath,
+			"vpc_name": "cci-test",
+			"public_key_path": publicKeyPath,
 		},
 	}
 
-	return terraformOptions, keyPair
-}
+	defer test_structure.RunTestStage(t, "teardown", func() {
+		terraform.Destroy(t, terraformOptions)
+	})
 
+	test_structure.RunTestStage(t, "setup", func() {
+		terraform.InitAndApply(t, terraformOptions)
+	})
+}
