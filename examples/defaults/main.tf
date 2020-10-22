64750b627b7a805fc6e96a91a1c7542f79b4451e
Richard Mah <dick.mah@gmail.com>
2020-10-22 15:21:42 -0400

    update example

--- examples/defaults/main.tf
+++ examples/defaults/main.tf
@@ -1,11 +1,19 @@
-variable "alicloud_region" {
-  default = "us-east-1"
-}
-
 provider "alicloud" {
   region = var.alicloud_region
 }
 
+module "vpc" {
+  source   = "git@github.com:insight-nervos/terraform-btcpool-alibaba-network.git?ref=dev"
+  vpc_name = var.vpc_name
+  num_azs  = 2
+}
+
 module "defaults" {
-  source = "../.."
+  source            = "../.."
+  security_group_id = module.vpc.node_security_group_id
+  vpc_id            = module.vpc.vpc_id
+  vswitch_id        = module.vpc.public_vswitch_ids[0]
+  root_volume_size  = "50"
+  create_eip        = true
+  public_key        = file(var.public_key_path)
 }
